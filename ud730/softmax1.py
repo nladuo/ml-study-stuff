768257868cac5e739f2c49d0350f15b39dd97128
nladuo <kalen25115@gmail.com>
2016-09-29 22:30:48 +0800

    new file:   ud730/softmax1.py

---
+++ ud730/softmax1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# scores = [1.0, 2.0, 3.0]
+scores = np.array([[1, 2, 3, 6],
+                   [2, 4, 5, 6],
+                   [3, 8, 7, 6]])
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    pass  # TODO: Compute and return softmax(x)
+    exp_x = np.exp(x)
+    return exp_x / np.sum(exp_x, axis=0)
+
+
+print(softmax(scores))
+
+# Plot softmax curves
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
